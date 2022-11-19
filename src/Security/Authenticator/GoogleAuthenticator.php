d8946ef161475ba1d52bdfabe9c3cdd456628663
Artur Szymczyk <arturszymczyk1@gmail.com>
2022-11-19 22:00:06 +0100

    chore: Fixes authenticator

--- src/Security/Authenticator/GoogleAuthenticator.php
+++ src/Security/Authenticator/GoogleAuthenticator.php
@@ -7,7 +7,6 @@ namespace App\Security\Authenticator;
 use App\Entity\User;
 use Doctrine\ORM\EntityManagerInterface;
 use KnpU\OAuth2ClientBundle\Client\OAuth2ClientInterface;
-use KnpU\OAuth2ClientBundle\Security\Authenticator\SocialAuthenticator;
 use KnpU\OAuth2ClientBundle\Client\ClientRegistry;
 use League\OAuth2\Client\Provider\GoogleUser;
 use Symfony\Component\HttpFoundation\Request;
@@ -15,9 +14,13 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
 use Symfony\Component\Security\Core\Exception\AuthenticationException;
 use Symfony\Component\Security\Core\User\UserProviderInterface;
+use Symfony\Component\Security\Http\Authenticator\AbstractAuthenticator;
+use Symfony\Component\Security\Http\Authenticator\Passport\Badge\UserBadge;
+use Symfony\Component\Security\Http\Authenticator\Passport\Passport;
+use Symfony\Component\Security\Http\Authenticator\Passport\SelfValidatingPassport;
 use Symfony\Component\Uid\Uuid;
 
-class GoogleAuthenticator extends SocialAuthenticator
+class GoogleAuthenticator extends AbstractAuthenticator
 {
     public function __construct(
         private readonly ClientRegistry $clientRegistry,
@@ -30,20 +33,11 @@ class GoogleAuthenticator extends SocialAuthenticator
         return $request->attributes->get('_route') === 'connect_auth_google_check';
     }
 
-    public function getCredentials(Request $request): \League\OAuth2\Client\Token\AccessToken
-    {
-        return $this->fetchAccessToken($this->getGoogleClient());
-    }
-
-    public function getUser($credentials, UserProviderInterface $userProvider)
+    private function getUser()
     {
         /** @var GoogleUser $googleUser */
-        $googleUser = $this->getGoogleClient()
-            ->fetchUserFromToken($credentials);
+        $googleUser = $this->getGoogleClient()->fetchUserFromToken($this->getGoogleClient()->getAccessToken());
 
-        $email = $googleUser->getEmail();
-
-        // 1) have they logged in with Facebook before? Easy!
         $existingUser = $this->em->getRepository(User::class)
             ->findOneBy(
                 [
@@ -87,5 +81,12 @@ class GoogleAuthenticator extends SocialAuthenticator
         $message = strtr($exception->getMessageKey(), $exception->getMessageData());
 
         return new Response($message, Response::HTTP_FORBIDDEN);
+    }
+
+    public function authenticate(Request $request): Passport
+    {
+        $user = $this->getUser();
+
+        return new SelfValidatingPassport(new UserBadge($user->email, fn () => $user));
     }
 }
